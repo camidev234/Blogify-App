029fe93cc10f4af1c92f56717c53abd4aa5cd440
Camilo Acevedo <camilo222andresace@gmail.com>
2023-10-08 20:50:17 -0500

    Auth user created

---
+++ ArticleManage/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Categorie(models.Model):
+    categorie = models.CharField(max_length=50)
+
+class Article(models.Model):
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class UserResponse(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    response = models.CharField(max_length=700)
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ Autenticacion/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+
+def signUpView(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('loginUser')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'signUp.html', {
+            'form': form 
+        })
+
+
+def loginUser(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            # Autenticar al usuario
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('indexLounge')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
